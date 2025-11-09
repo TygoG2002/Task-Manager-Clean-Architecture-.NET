c1ae30485d1bbe456d595770d1902e6ab509de4b
Tygo <tygo@tygodev.com>
2025-11-09 12:33:46 +0100

    Add ITaskRepository interface to Application layer

---
+++ CleanArchitecture.Application/Common/Interfaces/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Domain.Entities;
+
+namespace CleanArchitecture.Application.Common.Interfaces
+{
+    public interface ITaskRepository
+    {
+        Task<List<TaskItem>> GetAllAsync();
+        Task<TaskItem?> GetByIdAsync(int id);
+        Task AddAsync(TaskItem task);
+        Task UpdateAsync(TaskItem task);
+    }
+}
