f679cfc3cd1146e6048d62f0ee906309a04683e2
Tygo <tygo@tygodev.com>
2025-11-09 17:41:48 +0100

    added test for createdTask

---
+++ CleanArchitecture.Tests/Application/Features/Tasks/CreateTaskHandlerTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Features.Tasks;
+using CleanArchitecture.Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace CleanArchitecture.Tests.Application.Features.Tasks
+{
+    public class CreateTaskHandlerTests
+    {
+        [Fact]
+        public async Task Should_Create_Task_Successfully()
+        {
+            var mockRepo = new Mock<ITaskRepository>();
+            var handler = new CreateTaskHandler(mockRepo.Object);   
+            var command = new CreateTaskCommand { Title = "Workout" };
+
+            var result = await handler.Execute(command);
+
+            Assert.Equal("Workout", result.Title);
+            mockRepo.Verify(r => r.AddAsync(It.IsAny<TaskItem>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Should_Throw_When_Title_Is_Empty()
+        {
+            var mockRepo = new Mock<ITaskRepository>();
+            var handler = new CreateTaskHandler(mockRepo.Object);
+            var command = new CreateTaskCommand { Title = "" };
+
+            await Assert.ThrowsAsync<ArgumentException>(() => handler.Execute(command));
+        }
+    }
+}
---
+++ CreateTaskHandlerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Application.Features.Tasks;
+using CleanArchitecture.Domain.Entities;
+using Moq;
+using Xunit;
+
+namespace CleanArchitecture.Tests.Application.Features.Tasks
+{
+    public class CreateTaskHandlerTests
+    {
+        [Fact]
+        public async Task Should_Create_Task_Successfully()
+        {
+            // Arrange
+            var mockRepo = new Mock<ITaskRepository>();
+            var handler = new CreateTaskHandler(mockRepo.Object);
+            var command = new CreateTaskCommand { Title = "Workout" };
+
+            // Act
+            var result = await handler.Handle(command);
+
+            // Assert
+            Assert.Equal("Workout", result.Title);
+            mockRepo.Verify(r => r.AddAsync(It.IsAny<TaskItem>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task Should_Throw_When_Title_Is_Empty()
+        {
+            // Arrange
+            var mockRepo = new Mock<ITaskRepository>();
+            var handler = new CreateTaskHandler(mockRepo.Object);
+            var command = new CreateTaskCommand { Title = "" };
+
+            // Act & Assert
+            await Assert.ThrowsAsync<ArgumentException>(() => handler.Handle(command));
+        }
+    }
+}
